fe487f93aa06111cfa374a2fff23a8707efd8b49
Aditya-Damaraju <adityasarma234@gmail.com>
2022-01-09 23:37:58 +0530

    first commit

---
+++ javatpoint_25.py
@@ -0,0 +1,10 @@
+import math
+b_l = int(input("Enter the length of the Bridge: "))
+p_t = int(input("Enter the time to cross the Pole: "))
+b_t = int(input("Enter the time to cross the Bridge: "))
+x = print("Let x be the length of the train")
+y = print("Let y be the speed of the train")
+y = b_l / (b_t - p_t)
+speed = y * 18/5
+print("Speed of the train in Km/hr is ", speed)
+exit (0)
